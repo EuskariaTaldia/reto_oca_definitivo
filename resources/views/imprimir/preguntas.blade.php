831e95da34d578581040d4259bc4709c253fe35d
Nahia Maguregi <nahiamag19@gmail.com>
2020-02-03 16:51:01 +0100

    Juntando

--- resources/views/imprimir/preguntas.blade.php
+++ resources/views/imprimir/preguntas.blade.php
@@ -12,7 +12,7 @@
     <div class="fondo">
         <input class="btn btn-dark ancho" type="button" name="imprimir" value="Imprimir Datos" onclick="window.print();">
 
-        <h2>Listado modo carta</h2>
+        <h2>Preguntas modo carta</h2>
 
         {{-- LEYENDA --}}
         <h6 style="display: flex;justify-content: center;align-items: center;color:white">Leyenda</h6>
@@ -34,105 +34,109 @@
         <div class="row">
         {{-- Recorremos todas las mujeres --}}
         @foreach ($arrayPreguntas as $pregun)
-        <div class="col-6 columnas">
-        {{-- Carta Pregunta --}}
-        {{-- Se le pone fondo --}}
-        @if($pregun->mujer->codArea=="1")
-            <div class="historia card text-white bg-dark mb-3 tamañoCarta" style="max-width: 18rem;">
-        @elseif($pregun->mujer->codArea=="2")
-            <div class="derecho card text-white bg-dark mb-3 tamañoCarta" style="max-width: 18rem;">
-        @elseif($pregun->mujer->codArea=="3")
-            <div class="antropologia card text-white bg-dark mb-3 tamañoCarta" style="max-width: 18rem;">
-        @elseif($pregun->mujer->codArea=="4")
-            <div class="geografia card text-white bg-dark mb-3 tamañoCarta" style="max-width: 18rem;">
-        @elseif($pregun->mujer->codArea=="5")
-            <div class="filosofia card text-white bg-dark mb-3 tamañoCarta" style="max-width: 18rem;">
-        @elseif($pregun->mujer->codArea=="6")
-            <div class="psicologia card text-white bg-dark mb-3 tamañoCarta" style="max-width: 18rem;">
-        @elseif($pregun->mujer->codArea=="7")
-            <div class="economia card text-white bg-dark mb-3 tamañoCarta" style="max-width: 18rem;">
-        @elseif($pregun->mujer->codArea=="8")
-            <div class="sociologia card text-white bg-dark mb-3 tamañoCarta" style="max-width: 18rem;">
-        @elseif($pregun->mujer->codArea=="9")
-        <div class="pedagogia card text-white bg-dark mb-3 tamañoCarta" style="max-width: 18rem;">
-        @endif
-            <div class="circulo">
-            <img class="card-img-top border-left img-thumbnail imaTam" src="{{$pregun->mujer->fotografia}}" alt="Card image cap">
-            </div>
-            <div class="card-body">
-              <h5 class="card-title">{{$pregun->mujer->nombre}}</h5>
-              {{-- <h5 class="card-title">{{$pregun->mujer["nombre"]}}</h5> --}}
-              <p class="card-text">{{$pregun["pregunta"]}}</p>
-                <div id="respuestas" name="respuestas">
-                    <?php
-                    // Se imprimen las respuestas Ramdom
-                        $resC=$pregun["respuestacorrecta"];
-                        $resE1=$pregun["respuestaerronea1"];
-                        $resE2=$pregun["respuestaerronea2"];
-                        $respuestas=array($resC,$resE1,$resE2);
-                        // var_dump($respu);
-                        shuffle($respuestas);
-                        foreach ($respuestas as $respuesta) {
-                            if($respuesta==$resC){
-                                echo "<li type='circle'><u>$respuesta  </u><br></li>";
-                            }else {
-                                echo "<li type='circle'>$respuesta  <br></li>";
-                            }   
-                        }
+            <div class="col-6 columnas">
+                {{-- Carta Pregunta --}}
+                {{-- Se le pone fondo --}}
+                @if($pregun->mujer->codArea=="1")
+                    <div class="historia card text-white bg-dark mb-3 tamañoCarta" style="max-width: 18rem;">
+                @elseif($pregun->mujer->codArea=="2")
+                    <div class="derecho card text-white bg-dark mb-3 tamañoCarta" style="max-width: 18rem;">
+                @elseif($pregun->mujer->codArea=="3")
+                    <div class="antropologia card text-white bg-dark mb-3 tamañoCarta" style="max-width: 18rem;">
+                @elseif($pregun->mujer->codArea=="4")
+                    <div class="geografia card text-white bg-dark mb-3 tamañoCarta" style="max-width: 18rem;">
+                @elseif($pregun->mujer->codArea=="5")
+                    <div class="filosofia card text-white bg-dark mb-3 tamañoCarta" style="max-width: 18rem;">
+                @elseif($pregun->mujer->codArea=="6")
+                    <div class="psicologia card text-white bg-dark mb-3 tamañoCarta" style="max-width: 18rem;">
+                @elseif($pregun->mujer->codArea=="7")
+                    <div class="economia card text-white bg-dark mb-3 tamañoCarta" style="max-width: 18rem;">
+                @elseif($pregun->mujer->codArea=="8")
+                    <div class="sociologia card text-white bg-dark mb-3 tamañoCarta" style="max-width: 18rem;">
+                @elseif($pregun->mujer->codArea=="9")
+                    <div class="pedagogia card text-white bg-dark mb-3 tamañoCarta" style="max-width: 18rem;">
+                @endif
+                
+                <div class="circulo">
+                    <img class="card-img-top border-left img-thumbnail imaTam" src="{{$pregun->mujer->fotografia}}" alt="Card image cap">
+                </div>
 
-                        ?>
+                <div class="card-body">
+                    <h5 class="card-title">{{$pregun->mujer->nombre}}</h5>
+                    {{-- <h5 class="card-title">{{$pregun->mujer["nombre"]}}</h5> --}}
+                    <p class="card-text">{{$pregun["pregunta"]}}</p>
+                    
+                    <div id="respuestas" name="respuestas">
+                        <?php
+                        // Se imprimen las respuestas Ramdom
+                            $resC=$pregun["respuestacorrecta"];
+                            $resE1=$pregun["respuestaerronea1"];
+                            $resE2=$pregun["respuestaerronea2"];
+                            $respuestas=array($resC,$resE1,$resE2);
+                            // var_dump($respu);
+                            shuffle($respuestas);
+                            foreach ($respuestas as $respuesta) {
+                                if($respuesta==$resC){
+                                    echo "<li type='circle'><u>$respuesta  </u><br></li>";
+                                }else {
+                                    echo "<li type='circle'>$respuesta  <br></li>";
+                                }   
+                            }
+
+                            ?>
+                    </div>
                 </div>
             </div>
-          </div>
 
-          {{-- Carta Datos --}}
-            {{-- Se le pone fondo --}}
-          @if($pregun->mujer->codArea=="1")
-            <div class="historia card text-white bg-dark mb-3 tamañoCarta" style="max-width: 18rem;">
-        @elseif($pregun->mujer->codArea=="2")
-            <div class="derecho card text-white bg-dark mb-3 tamañoCarta" style="max-width: 18rem;">
-        @elseif($pregun->mujer->codArea=="3")
-            <div class="antropologia card text-white bg-dark mb-3 tamañoCarta" style="max-width: 18rem;">
-        @elseif($pregun->mujer->codArea=="4")
-            <div class="geografia card text-white bg-dark mb-3 tamañoCarta" style="max-width: 18rem;">
-        @elseif($pregun->mujer->codArea=="5")
-            <div class="filosofia card text-white bg-dark mb-3 tamañoCarta" style="max-width: 18rem;">
-        @elseif($pregun->mujer->codArea=="6")
-            <div class="psicologia card text-white bg-dark mb-3 tamañoCarta" style="max-width: 18rem;">
-        @elseif($pregun->mujer->codArea=="7")
-            <div class="economia card text-white bg-dark mb-3 tamañoCarta" style="max-width: 18rem;">
-        @elseif($pregun->mujer->codArea=="8")
-            <div class="sociologia card text-white bg-dark mb-3 tamañoCarta" style="max-width: 18rem;">
-        @elseif($pregun->mujer->codArea=="9")
-        <div class="pedagogia card text-white bg-dark mb-3 tamañoCarta" style="max-width: 18rem;">
-        @endif
+            {{-- Carta Datos --}}
+                {{-- Se le pone fondo --}}
+            @if($pregun->mujer->codArea=="1")
+                <div class="historia card text-white bg-dark mb-3 tamañoCarta" style="max-width: 18rem;">
+            @elseif($pregun->mujer->codArea=="2")
+                <div class="derecho card text-white bg-dark mb-3 tamañoCarta" style="max-width: 18rem;">
+            @elseif($pregun->mujer->codArea=="3")
+                <div class="antropologia card text-white bg-dark mb-3 tamañoCarta" style="max-width: 18rem;">
+            @elseif($pregun->mujer->codArea=="4")
+                <div class="geografia card text-white bg-dark mb-3 tamañoCarta" style="max-width: 18rem;">
+            @elseif($pregun->mujer->codArea=="5")
+                <div class="filosofia card text-white bg-dark mb-3 tamañoCarta" style="max-width: 18rem;">
+            @elseif($pregun->mujer->codArea=="6")
+                <div class="psicologia card text-white bg-dark mb-3 tamañoCarta" style="max-width: 18rem;">
+            @elseif($pregun->mujer->codArea=="7")
+                <div class="economia card text-white bg-dark mb-3 tamañoCarta" style="max-width: 18rem;">
+            @elseif($pregun->mujer->codArea=="8")
+                <div class="sociologia card text-white bg-dark mb-3 tamañoCarta" style="max-width: 18rem;">
+            @elseif($pregun->mujer->codArea=="9")
+                <div class="pedagogia card text-white bg-dark mb-3 tamañoCarta" style="max-width: 18rem;">
+            @endif
+                
             <div class="card-body">
-                {{-- Se imprimen los datos con una relacion entre tablas --}}
-              <p class="card-title sinEspacio 
-              @if($pregun->mujer->codArea=="2" || $pregun->mujer->codArea=="8")
+                    {{-- Se imprimen los datos con una relacion entre tablas --}}
+                <p class="card-title sinEspacio 
+                @if($pregun->mujer->codArea=="2" || $pregun->mujer->codArea=="8")
+                    colorLetra
+                    @endif
+                    "><b><u>{{$pregun->mujer->nombre}}</u></b></p>
+                <p class="card-text sinEspacio 
+                @if($pregun->mujer->codArea=="2" || $pregun->mujer->codArea=="8")
                 colorLetra
                 @endif
-                "><b><u>{{$pregun->mujer->nombre}}</u></b></p>
-              <p class="card-text sinEspacio 
-              @if($pregun->mujer->codArea=="2" || $pregun->mujer->codArea=="8")
-              colorLetra
-              @endif
-              ">({{$pregun->mujer->fechas}})</p>
-              <p class="card-text 
-              @if($pregun->mujer->codArea=="2" || $pregun->mujer->codArea=="8")
-              colorLetra
-              @endif
-              ">{{$pregun->mujer->zona}}</p>
-              <p class="card-text tamañoLetra  
-              @if($pregun->mujer->codArea=="2" || $pregun->mujer->codArea=="8")
-              colorLetra
-              @endif
-              ">{{$pregun->mujer->datos}}</p>
+                ">({{$pregun->mujer->fechas}})</p>
+                <p class="card-text 
+                @if($pregun->mujer->codArea=="2" || $pregun->mujer->codArea=="8")
+                colorLetra
+                @endif
+                ">{{$pregun->mujer->zona}}</p>
+                <p class="card-text tamañoLetra  
+                @if($pregun->mujer->codArea=="2" || $pregun->mujer->codArea=="8")
+                colorLetra
+                @endif
+                ">{{$pregun->mujer->datos}}</p>
 
-              
+                
             </div>
-          </div>
-        </div>
+            </div>
+            </div>
         @endforeach
                  
            
